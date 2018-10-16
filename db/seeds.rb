1643b967f357bdd91477361ee00438df16b1d757
Daniel Lessa <daniellsf@id.uff.br>
2018-10-16 10:39:09 -0300

    Criando tabela de usuarios

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+20.times do |index|
+    User.create(
+        first_name: Faker::Name.name,
+        last_name: Faker::Name.last_name,
+        email: "email#{index}@email.com"
+    )
+end   
