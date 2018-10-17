c947af9a80d16608a29c5e293c40d1f90acd4e57
Daniel Lessa <daniellsf@id.uff.br>
2018-10-16 23:02:32 -0300

    Removendo comment id

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   patch '/topics/:topic_id/dislike/:user_id', to: 'topics#dislike'
   patch '/topics/:topic_id/like/:user_id', to: 'topics#like'
   get '/topics/:topic_id/comments', to: 'topics#comment_index'
-  post '/topics/:topic_id/comments/:comment_id', to: 'topics#comment_create'
+  post '/topics/:topic_id/comments', to: 'topics#comment_create'
   delete '/topics/:topic_id/comments/:comment_id', to: 'topics#comment_delete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
